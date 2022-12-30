96efa1154329edebaec1cc4aaaef7616e509e5c4
samoatesgames <sam@samoatesgames.com>
2022-12-30 11:49:34 +0000

    Basic lambert lighting

--- Open3DViewer.Gui/PBRRenderEngine/Shaders/Assets/ObjectShader/ObjectShader.frag.glsl
+++ Open3DViewer.Gui/PBRRenderEngine/Shaders/Assets/ObjectShader/ObjectShader.frag.glsl
@@ -1,6 +1,8 @@
 ﻿#version 450
 
-layout(location = 0) in vec2 fsin_texCoords;
+layout(location = 0) in vec3 fsin_normal;
+layout(location = 1) in vec2 fsin_texCoords;
+
 layout(location = 0) out vec4 fsout_color;
 
 layout(set = 2, binding = 1) uniform sampler DiffuseSampler;
@@ -18,14 +20,30 @@ layout(set = 5, binding = 2) uniform texture2D EmissiveTexture;
 layout(set = 6, binding = 1) uniform sampler OcclusionSampler;
 layout(set = 6, binding = 2) uniform texture2D OcclusionTexture;
 
+float lambert(vec3 N, vec3 L)
+{
+  vec3 nrmN = normalize(N);
+  vec3 nrmL = normalize(L);
+  float result = dot(nrmN, nrmL);
+  return max(result, 0.0);
+}
+
 void main()
 {
-    vec4 diffuse = texture(sampler2D(DiffuseTexture, DiffuseSampler), fsin_texCoords);
-    vec4 normal = texture(sampler2D(NormalTexture, NormalSampler), fsin_texCoords);
-    vec4 roughness = texture(sampler2D(MetallicRoughnessTexture, MetallicRoughnessSampler), fsin_texCoords);
-    vec4 emissive = texture(sampler2D(EmissiveTexture, EmissiveSampler), fsin_texCoords);
-    vec4 occlusion = texture(sampler2D(OcclusionTexture, OcclusionSampler), fsin_texCoords);
+    vec4 diffuseTexture = texture(sampler2D(DiffuseTexture, DiffuseSampler), fsin_texCoords);
+    vec4 normalTexture = texture(sampler2D(NormalTexture, DiffuseSampler), fsin_texCoords);
+    vec4 roughnessTexture = texture(sampler2D(MetallicRoughnessTexture, MetallicRoughnessSampler), fsin_texCoords);
+    vec4 emissiveTexture = texture(sampler2D(EmissiveTexture, EmissiveSampler), fsin_texCoords);
+    vec4 occlusionTexture = texture(sampler2D(OcclusionTexture, OcclusionSampler), fsin_texCoords);
 
-    //fsout_color = diffuse + normal + roughness + emissive + occlusion;
-    fsout_color = diffuse;
+    // TODO: Provide lights from buffers
+    vec3 lightDirection = vec3(-2,1,-4);
+    vec3 lightDiffuse = vec3(1.0, 1.0, 1.0);
+    vec3 lighting = lightDiffuse * lambert(fsin_normal, lightDirection);
+
+    // TODO: Provide ambiant amount from buffer
+    vec3 ambiantLight = vec3(0.4, 0.4, 0.4);
+
+    vec3 result = (clamp(ambiantLight + lighting, 0, 1) * diffuseTexture.xyz);       
+    fsout_color = vec4(result, 1.0f);
 }
--- Open3DViewer.Gui/PBRRenderEngine/Shaders/Assets/ObjectShader/ObjectShader.vert.glsl
+++ Open3DViewer.Gui/PBRRenderEngine/Shaders/Assets/ObjectShader/ObjectShader.vert.glsl
@@ -11,11 +11,13 @@ layout(set = 1, binding = 0) uniform WorldBuffer
 {
     mat4 World;
 };
+
 layout(location = 0) in vec3 Position;
 layout(location = 1) in vec3 Normal;
 layout(location = 2) in vec2 TexCoords;
 
-layout(location = 0) out vec2 fsin_texCoords;
+layout(location = 0) out vec3 fsin_normal;
+layout(location = 1) out vec2 fsin_texCoords;
 
 void main()
 {
@@ -24,5 +26,6 @@ void main()
     vec4 clipPosition = Projection * viewPosition;
     gl_Position = clipPosition;
     
+    fsin_normal = Normal;
     fsin_texCoords = TexCoords;
 }
