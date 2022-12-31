30560ad5dcbc3a38afc3814d9fc04c5640e1b181
samoatesgames <sam@samoatesgames.com>
2022-12-31 16:50:10 +0000

    Add bump mapping

--- Open3DViewer.Gui/PBRRenderEngine/Shaders/Assets/ObjectShader/ObjectShader.frag.glsl
+++ Open3DViewer.Gui/PBRRenderEngine/Shaders/Assets/ObjectShader/ObjectShader.frag.glsl
@@ -1,10 +1,5 @@
 ﻿#version 450
 
-layout(location = 0) in vec3 fsin_normal;
-layout(location = 1) in vec2 fsin_texCoords;
-
-layout(location = 0) out vec4 fsout_color;
-
 layout(set = 2, binding = 0) uniform sampler DiffuseSampler;
 layout(set = 2, binding = 1) uniform texture2D DiffuseTexture;
 layout(set = 2, binding = 2) uniform DiffuseMaterial
@@ -19,6 +14,11 @@ layout(set = 3, binding = 2) uniform NormalMaterial
     vec4 NormalTint;
 };
 
+layout(location = 0) in vec3 fsin_normal;
+layout(location = 1) in vec2 fsin_texCoords;
+
+layout(location = 0) out vec4 fsout_color;
+
 float lambert(vec3 N, vec3 L)
 {
   vec3 nrmN = normalize(N);
@@ -30,16 +30,32 @@ float lambert(vec3 N, vec3 L)
 void main()
 {
     // TODO: Provide lights from buffers
-    vec3 lightDirection = vec3(-2,1,-4);
+    vec3 lightDirection = normalize(vec3(-2,1,-4));
     vec3 lightDiffuse = vec3(1.0, 1.0, 1.0);
     vec3 lighting = lightDiffuse * lambert(fsin_normal, lightDirection);
 
     // TODO: Provide ambiant amount from buffer
-    vec3 ambiantLight = vec3(0.6, 0.55, 0.5);
+    vec3 ambiantLight = vec3(0.8, 0.75, 0.7);
 
+    // Diffuse color
     vec4 diffuseTexture = texture(sampler2D(DiffuseTexture, DiffuseSampler), fsin_texCoords);
     vec4 diffuse = diffuseTexture * DiffuseTint;
 
-    vec3 result = (clamp(ambiantLight + lighting, 0, 1) * diffuse.xyz);       
-    fsout_color = vec4(result, 1.0f);
+    // Extract the normal from the normal map  
+    vec4 normalTexture = texture(sampler2D(NormalTexture, NormalSampler), fsin_texCoords);
+    vec3 normal = normalize(normalTexture.rgb * 2.0 - 1.0);
+    float normalDiffuse = max(dot(normal, -lightDirection), 0.0);
+    vec3 bumpedDiffuse = normalDiffuse * diffuse.rgb;
+    
+    // Final result
+    vec3 final = (clamp(ambiantLight + lighting, 0, 10) * bumpedDiffuse);
+    vec3 result = final;
+    
+    // [Debug] Draw Diffuse Map Only
+    //result = (final - final) + diffuseTexture.rgb;
+
+    // [Debug] Draw Normal Map Only
+    //result = (final - final) + normalTexture.rgb;
+    
+    fsout_color = vec4(result, 1);
 }
